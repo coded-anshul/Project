960082baf6d3ed09dea963b734ba21a93bbb5a73
coded-anshul <anshuljain2106@gmail.com>
2025-09-14 15:10:40 +0530

    check

--- quiz.js
+++ quiz.js
@@ -1,4 +1,23 @@
-const option = document.querySelector()
-const btn= document.addEventListener()
+const option = document.querySelector('button');
+const btn= document.addEventListener();
+var a="";
 
+const right = document.querySelector('.right');
+const wrong=document.querySelector('.wrong');
+right.addEventListener('click',()=>{
+    a=true;
+}
+)
 
+wrong.addEventListener('click',()=>{
+    a=false;
+})
+
+option.addEventListener('click',()=>{
+    if (a=true){
+        option.style.backgroundColor='green';
+    }
+    else{
+        option.style.backgroundColor='red';
+    }
+})
